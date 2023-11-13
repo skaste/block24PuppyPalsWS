ebf0f97ddf35a443797c0de7ab2665d0b7416cc8
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-13 15:42:12 -0600

    finished up the second block of workshop

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -1,14 +1,19 @@
-import { puppyList } from "./data"
-import { useState } from "react"
+import { puppyList } from "./data";
+import { useState } from "react";
 
 function App() {
-console.log(puppyList)
-const [] = useState()
+
+const [puppies, setPuppies] = useState(puppyList);
+console.log("puppyList:", puppyList);
   return (
    <>
-   <h2>Hello World!!</h2>
+   {
+   puppies.map((puppy) => {
+    return <p key={puppy.id}>{puppy.name}</p>;
+    })
+    }
    </>
-  )
+  );
 }
 
 export default App
