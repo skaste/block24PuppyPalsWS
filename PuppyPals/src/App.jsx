0d03b692e2075b0f670630da28169398a2907c11
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-13 16:12:04 -0600

    set puppyID in p tag

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -4,14 +4,16 @@ import { useState } from "react";
 function App() {
 
 const [puppies, setPuppies] = useState(puppyList);
+const [featPupId, setFetPupId] = useState(null);
 console.log("puppyList:", puppyList);
   return (
    <>
    {
    puppies.map((puppy) => {
-    return <p key={puppy.id}>{puppy.name}</p>;
+    return <p onClick={()=>{setFetPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
     })
     }
+    <p>{featPupId}</p>
    </>
   );
 }
